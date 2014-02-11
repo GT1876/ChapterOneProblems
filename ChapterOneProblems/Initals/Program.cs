d89c3e659450147ee72dbb44398769a88bda1e9c
GT1876 <garrysontime@gmail.com>
2014-02-11 10:11:20 -0500

    Chips

---
+++ ChapterOneProblems/Initals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Initals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "GGGGGGG         TTTTTTTTTTTTT");
+            Console.WriteLine( "GG                    TT");
+            Console.WriteLine( "GG                    TT");
+            Console.WriteLine( "GG                    TT");
+            Console.WriteLine( "GG    GGGGG           TT"); 
+            Console.WriteLine( "GG      G             TT");
+            Console.WriteLine( "GGGGGGGGG             TT");
+
+        }
+    }
+}
